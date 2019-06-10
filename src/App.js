24a404860487e3d8763460125f7c4021f282a55c
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-06-10 15:41:29 +0530

    added header and footer content and implemented router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./components/HomePage/HomePage";
 
-function App() {
-  console.log(process.env.REACT_APP_RECIPE_SEARCH_APP_KEY);
-  return (
-    <div className="App">
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log(process.env.REACT_APP_RECIPE_SEARCH_APP_KEY);
+    return (
+      <div className="main-app">
+        <Router>
+          <Header {...this.props} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route render={() => <Redirect to={{ pathname: "/" }} />} />
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import "./header.css";
+
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="container p-0">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <span className="nav-link p-0">
+              <Link className="pl-2" to="/">
+                <img
+                  src={require("../../assets/images/breakfast.png")}
+                  alt="brand-logo"
+                  width="40px"
+                />
+                <span className="pl-2 pt-2">Food Recipe App</span>
+              </Link>
+            </span>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbar-div"
+              aria-controls="navbar-div"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbar-div">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item dropdown">
+                  <span
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Services
+                  </span>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <span className="dropdown-item nav-link">
+                      <Link to="">Recipe Search</Link>
+                    </span>
+                    <span className="dropdown-item nav-link">
+                      <Link to="">Food Database</Link>
+                    </span>
+                    <span className="dropdown-item nav-link">
+                      <Link to="">Nutrition Analysis</Link>
+                    </span>
+                  </div>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link">
+                    <Link to="">About</Link>
+                  </span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link">
+                    <Link to="">Contact</Link>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;

51d6e3b51ea1ecb25649e50f1b26efa8a149fdc3
rishav-pandey <27219302+rishav-pandey@users.noreply.github.com>
2019-07-05 19:42:33 +0530

    Updates

---
+++ precache-manifest.a42b431a1373ab8c6c08d7270aa6570a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6fb39835ad66c1fe219748d9542cd4",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "c50b49d9310ebbcb839b",
+    "url": "/food-recipe-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2b948d978fd371e580e5",
+    "url": "/food-recipe-app/static/css/main.378c861b.chunk.css"
+  },
+  {
+    "revision": "c50b49d9310ebbcb839b",
+    "url": "/food-recipe-app/static/js/2.840088ba.chunk.js"
+  },
+  {
+    "revision": "2b948d978fd371e580e5",
+    "url": "/food-recipe-app/static/js/main.36713e0b.chunk.js"
+  },
+  {
+    "revision": "5bddeccd37c07d7d0169",
+    "url": "/food-recipe-app/static/js/runtime~main.5a780165.js"
+  },
+  {
+    "revision": "fd5013a89c1c09578d1e245b5076e565",
+    "url": "/food-recipe-app/static/media/under-512.fd5013a8.png"
+  }
+]);
